66ae92497a735e3a1c62b1a49637636bd1f1ee70
failoobmen <49118083+failoobmen@users.noreply.github.com>
2024-11-16 22:51:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ include_once 'db.class.php';
 define('db_host','localhost');
 
 // Имя БД
-define('matsievskij','name');
+define('db_name','matsievskij');
 
 // Пользователь БД
-define('db_user','root');
+define('db_user','admin');
 
 // Пароль БД
-define('db_pass','password');
+define('db_pass','admin_password');
 
 // Обявление класса для подключения к бд
 $db = new DB_class(db_host,db_name,db_user,db_pass);
