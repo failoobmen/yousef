4444c6212ad83728ac34d73f2c2d22ae909dd025
failoobmen <49118083+failoobmen@users.noreply.github.com>
2024-11-17 03:07:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 include_once 'db.class.php';
 
 // хост БД
-define('db_host','localhost');
+define('db_host','127.0.0.1');
 
 // Имя БД
 define('db_name','matsievskij');
 
 // Пользователь БД
-define('db_user','root');
+define('db_user','admin');
 
 // Пароль БД
 define('db_pass','admin_password');
@@ -17,5 +17,6 @@ define('db_pass','admin_password');
 // Обявление класса для подключения к бд
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 
-$query = $db->select(true,"*","matsievskij","task_id='1'");
+$query = $db->select(true,"*","matsievskij");
+print_r($query);
 ?>
