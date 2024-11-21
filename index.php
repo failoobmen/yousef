37163bae263ff580d46887d9fea81b6666dc0597
failoobmen <49118083+failoobmen@users.noreply.github.com>
2024-11-21 18:42:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,33 @@ define('db_pass','admin_password');
 // Обявление класса для подключения к бд
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 
-$query = $db->select(true,"*","matsievskij");
-print_r($query);
+$query = $db->select(true,"*","matsievskij","");
+
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Мой задачник</title>
+      <link rel="stylesheet" type="text/css" href="style.css"
+  </head>
+  <body>
+  <div class="tasks">
+      <?php
+      for ($i = 0; $i < count($query);$i++){
+      ?>
+    
+  <div class="task-control">
+  <input type="checkbox" name="">
+  </div>
+  <div class="task-text"><?php echo $query[$i]['task_text'] ?></div>
+  <div class="task-date">
+  <input type="date" name=''>
+  </div>
+  </div>
+          <?php
+  }
+  ?>
+  </div>
+  </body>
+
+</html>
